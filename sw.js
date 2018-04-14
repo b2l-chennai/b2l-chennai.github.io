af4709ee3e4e269869c5131aaaf9164a3cfe70a2
Shriram Balaji <>
2018-04-14 19:48:12 +0530

    Fixed visible layout and styling issues
    - Added header images from unsplash
    - Updated about.md with additional content
    - Fixed responsive behaviour of website
    - Added current project as the first project in Projects
    - Colors updated

--- sw.js
+++ sw.js
@@ -10,10 +10,11 @@ var urlsToCache = [
     '/manifests/manifest_webapp.json',
     '/manifests/manifest_notpush.json',
     '/static/img/header-home.jpg',
+    '/static/img/header-blog.jpg',
+    '/static/img/header-collab.jpg'
+    '/static/img/header-default.png'
     '/static/img/balloon_1.png',
     '/static/img/balloon_2.png',
-    '/static/img/balloon_3.png',
-    '/static/img/balloon_4.png',
     '/static/img/logo.png',
     '/static/css/main.css',
     '/static/css/bootstrap-material-design.min.css',
